7a7ffd52ceca19ab182ee47111b490d797ca59c0
Artin <artin.phares@gmail.com>
2015-05-22 22:08:55 +0000

    #67 Test infrastructure 2

--- build/make.cmd
+++ build/make.cmd
@@ -1,5 +1,16 @@
 cd ../tests
-gulp tests || { echo "Tests failed!"; exit 1; }
+echo "Static code analysis..."
+gulp static-analysis || { echo "Static code analysis failed!"; exit 1; }
+echo "Static code analysis done." && echo
 
 cd ../build
+echo "Building..."
 gulp build || { echo "Build failed!"; exit 1; }
+echo "Build done." && echo
+
+cd ../tests
+echo "Server tests..."
+gulp server || { echo "Server tests failed!"; exit 1; }
+echo "Server tests done." && echo
+
+echo "Successfully done." 
--- build/make.sh
+++ build/make.sh
@@ -1,5 +1,16 @@
-cd ../tests
-gulp tests || { echo "Tests failed!"; exit 1; }
+echo "Static code analysis..."
+cd ../test
+gulp test-static || { echo "Static code analysis failed!"; exit 1; }
+echo "Static code analysis done." && echo
 
+echo "Building..."
 cd ../build
 gulp build || { echo "Build failed!"; exit 1; }
+echo "Build done." && echo
+
+echo "Server tests..."
+cd ../test
+gulp test-server || { echo "Server tests failed!"; exit 1; }
+echo "Server tests done." && echo
+
+echo "Successfully done." 
