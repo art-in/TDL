2c6a2b4dd928b46c345e59db211a85dfebe138ac
artin-phares <artin.phares@gmail.com>
2016-02-23 23:46:37 +0300

    win cmds

---
+++ build/make_and_test.cmd
@@ -0,0 +1,16 @@
+echo "Static code analysis..."
+cd ../test
+gulp static || { echo "Static code analysis failed!"; exit 1; }
+echo "Static code analysis done." && echo
+
+echo "Building..."
+cd ../build
+gulp build || { echo "Build failed!"; exit 1; }
+echo "Build done." && echo
+
+echo "Server tests..."
+cd ../test
+gulp server || { echo "Server tests failed!"; exit 1; }
+echo "Server tests done." && echo
+
+echo "Successfully done." 
