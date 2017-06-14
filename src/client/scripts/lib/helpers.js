68b880c4f78e38502f58017864341dda7a9f49be
artin-phares <artin.phares@gmail.com>
2017-06-14 14:54:42 +0300

    #89 fixed URL parsing

--- src/client/scripts/lib/helpers.js
+++ src/client/scripts/lib/helpers.js
@@ -81,7 +81,7 @@ define(function() {
         result = unwrapUrls(result);
 
         // wrap URLs into links
-        var urlRegex = /https?:\/\/((www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/g;
+        var urlRegex = /https?:\/\/((www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=…;]*))/g;
         result = result.replace(urlRegex, function(url, urlWithoutScheme) {
 
             var innerText = urlWithoutScheme;
